98f60de4f92b95ccdeb1ff372872f97fab5e4556
Ben Rauzi <ben.rauzi@outlook.com>
2020-12-03 19:50:23 +1300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: `${process.env.ORIGIN || "http://localhost:3000"}`,
     credentials: true
 }));
 
