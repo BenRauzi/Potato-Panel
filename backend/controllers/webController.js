465d124517f1e8fce5831872bbb6caf8216f1a57
Ben Rauzi <ben.rauzi@outlook.com>
2020-12-07 12:25:40 +1300

    Add Website Whitelisting for Light

--- backend/controllers/webController.js
+++ backend/controllers/webController.js
@@ -12,10 +12,38 @@ const webController = (app, sqlAsync) => {
             const userInfo = await sqlAsync.awaitQuery("SELECT uid, name, cash, bankacc, playtime from players where pid = ?", [id])
             res.send(userInfo)
         } catch (error) {
-            console.log(error)
             res.sendStatus(500)   
         }
-        
+    })
+
+    app.post('/user/cop-whitelist', async(req, res) => {
+        const id = req.body.pid
+
+        const key = req.headers["x-api-key"] || undefined
+
+        if(key !== process.env.WEB_API_KEY) return res.send(401)
+
+        try {
+            const userInfo = await sqlAsync.awaitQuery("UPDATE players SET copLevel = 1, copdept = 1 WHERE pid = ?", [id])
+            res.sendStatus(200)
+        } catch (error) {
+            res.sendStatus(500)   
+        }
+    })
+
+    app.post('/user/ems-whitelist', async(req, res) => {
+        const id = req.body.pid
+
+        const key = req.headers["x-api-key"] || undefined
+
+        if(key !== process.env.WEB_API_KEY) return res.send(401)
+
+        try {
+            await sqlAsync.awaitQuery("UPDATE players SET medicLevel = 1, medicdept = 1 WHERE pid = ?", [id])
+            res.sendStatus(200)
+        } catch (error) {
+            res.sendStatus(500)   
+        }
     })
 }
 
