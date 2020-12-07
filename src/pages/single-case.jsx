41cbf00d10c2af2fe814ab49e8b696573d54081b
Ben Rauzi <ben.rauzi@outlook.com>
2020-12-07 14:46:11 +1300

    Add Support Cases to API and Basic Frontend

---
+++ src/pages/single-case.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "../components/title";
+
+const SingleCasePage = () => {
+    return (
+        <>
+            <Title title="Single Case"/>
+            Single Case
+        </>
+    )
+}
+
+export default SingleCasePage;
