37b293eb7cf31b22c8a3d61212a133f44582587f
Ben Rauzi <ben.rauzi@outlook.com>
2020-12-15 16:30:27 +1300

    Increase Cookie MaxAge

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,7 +41,7 @@ const authController = (app, sql, sqlAsync) => {
                         
                     }, process.env.JWT_SECRET);
                     // save token in cookie
-                    res.cookie('authcookie',token,{maxAge:1000*60*60,httpOnly:true});
+                    res.cookie('authcookie',token,{maxAge:1000*60*60*60,httpOnly:true});
 
                     res.send({...result[0], password: undefined});
                 } else {
