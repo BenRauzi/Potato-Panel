1b595fb06da5de0e0ef4a6fb4a12bd71ce2567fd
Ben Rauzi <ben.rauzi@outlook.com>
2020-12-21 19:16:43 +1300

    Move to new RCON Client. Configure for Development Environment

--- rcon-client/services/authService.js
+++ rcon-client/services/authService.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 
 function checkToken (req, res, next) {
     // get authcookie from request
+    console.log(req.cookies)
     const authcookie = req.cookies.authcookie
 
     // verify token which is in cookie value
     jwt.verify(authcookie, process.env.JWT_SECRET,(err,data)=>{
         if(err){
-            console.log(err)
             res.clearCookie("authcookie")
             res.sendStatus(403)
         }
